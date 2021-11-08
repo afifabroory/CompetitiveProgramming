5bcd5d8961ccd2518c6236f5bab9358eb733b154
afifabroory <afif.abroory@gmail.com>
2021-11-08 14:31:39 +0800

    TOKI

---
+++ toki/ngoding-seru-2015-oct-pemula/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    unsigned short x, y;
+    cin >> x >> y;
+
+    cout << x + y << "\n";
+    cout << x - y << "\n";
+    cout << x * y << "\n";
+    cout << x / y << "\n";
+    cout << x % y << "\n";
+
+    return 0;
+}
