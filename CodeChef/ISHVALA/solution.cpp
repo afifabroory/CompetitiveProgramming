c0516d0d8b124c67c4d083349343faa09e3d66e0
afifabroory <afifabroory@gmail.com>
2022-01-02 15:58:07 +0800

    CodeChef: ISHVALA (Remove unnecessary array)

--- CodeChef/ISHVALA/solution.cpp
+++ CodeChef/ISHVALA/solution.cpp
@@ -16,22 +16,22 @@ void solve() {
     unsigned n, m, x, y, s;
     cin >> n >> m >> x >> y >> s;
 
-    unsigned x_i[x] = {0}, y_i[y] = {0};
+    unsigned x_i, y_i;
     unsigned grid_x = 0, grid_y = 0, prev_x = 0, prev_y = 0;
     
     // Horizontal river
     for (unsigned i = 0; i < x; i++) {
-        cin >> x_i[i];
-        grid_y += (x_i[i]-1 - prev_y)/s;
-        prev_y = x_i[i];
+        cin >> x_i;
+        grid_y += (x_i-1 - prev_y)/s;
+        prev_y = x_i;
     }
     grid_y += (n - prev_y)/s;
     
     // Vertical river
     for (unsigned i = 0; i < y; i++) {
-        cin >> y_i[i];
-        grid_x += (y_i[i]-1 - prev_x)/s;
-        prev_x = y_i[i];
+        cin >> y_i;
+        grid_x += (y_i-1 - prev_x)/s;
+        prev_x = y_i;
     }
     grid_x += (m - prev_x)/s;
 
