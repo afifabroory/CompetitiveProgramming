9602fe98038989825f7cd70eafb39f5b8ae18d88
Muhammad Afif Abroory <afifabroory@gmail.com>
2022-06-11 01:56:10 +0000

    Update random pick to support new CodeChef feature

--- CodeChef/pick_problem_random.js
+++ CodeChef/pick_problem_random.js
@@ -1,2 +1,5 @@
 // Run this on Console
 for(problem=document.querySelector("tbody").children,done=document.querySelectorAll('[title="Accepted"]'),pick=problem[Math.floor(Math.random()*problem.length)],not_found=!0;not_found;){pick=problem[Math.floor(Math.random()*problem.length)];for(var i=0;i<done.length;i++){if(done[i].parentElement.parentElement.parentElement.isEqualNode(pick)){not_found=!0;break}not_found=!1}}console.log(pick.children[2].children[1].children[0].href);
+
+// Run this on Console (Unattended tab)
+problem=document.querySelector("tbody").children;pick=problem[Math.floor(Math.random()*problem.length)];console.log(pick.children[2].children[1].children[0].href);
